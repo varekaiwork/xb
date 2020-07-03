a6df83b0ec65480d2789b0b93678d71b835a28b4
varekaiwork <serhiisaviukwork@gmail.com>
2020-07-03 16:48:12 +0300

    add opinion section

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,9 @@
-
-$(document).ready(function(){
- 
-  $('.slider__cont').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-         
-          slidesToShow: 3
-          
-        }
-      },
-      {
-        breakpoint: 660,
-        settings: {
-         
-          slidesToShow: 4
-          
-        }
-      },
-      {
-        breakpoint: 510,
-        settings: {
-         
-          slidesToShow: 3
-          
-        }
-      },
-      {
-        breakpoint: 430,
-        settings: {
-         
-          slidesToShow: 2
-          
-        }
-      }
-      
-      
-      
-    ]
-  
-  
+$(document).ready(function () {
+  $('.users__slider').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
   });
-  
-  
-
 });
