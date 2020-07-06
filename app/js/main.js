c5261d2cab11c2312d008380e6e1b3bff5703527
varekaiwork <serhiisaviukwork@gmail.com>
2020-07-06 14:06:18 +0300

    slick slider[

--- app/js/main.js
+++ app/js/main.js
@@ -7,26 +7,7 @@ $(document).ready(function () {
       $('body, html').animate({ scrollTop: scrollTo + 'px' }, 800);
     }
   });
-  $('.users__slider').slick({
-    infinite: true,
-    dots: true,
-    arrows: false,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 1250,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
 
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ],
-  });
   $('.header__btn').click(function () {
     $('.header__btn').toggleClass('active');
     $('.menu__list').toggleClass('open');
